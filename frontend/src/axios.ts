f38117640b6337445848ede592a8110c62ee9e9c
elliott717 <elliotthicks717@gmail.com>
2023-10-23 16:44:53 -0400

    initial project

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:APIPORT", //The APIPORT should be replaced with the API Port on the backend
+  baseURL: "http://localhost:3001", //The APIPORT should be replaced with the API Port on the backend
   headers: {
     "Content-type": "application/json"
   }
